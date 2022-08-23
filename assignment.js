646b3b859d6606cba7adc93d21951fa6ccf1c00e
riazul78 <riazulrakib@gmail.com>
2022-08-23 16:35:36 +0600

    alert-part-added

--- assignment.js
+++ assignment.js
@@ -1,6 +1,6 @@
 const playersArr =[];
 function display(playersHere){
-    const playersList = document.getElementById('players-list');
+    const playersList = document.getElementById('ol');
     playersList.innerHTML = "";
     for(let i = 0; i < playersHere.length; i++){
            
@@ -15,10 +15,21 @@ function display(playersHere){
 function selectPlayer(player){
     const playerName =player.parentNode.children[0].innerText;
     
-    const players = {
+    if (playersArr.length >= 5) {
+        alert('maximum players added');
+        const ol = document.getElementById('ol li');
+        ol.style.display = 'none';
+        
+    }
+    else {
+        const playerBtn = player;
+        playerBtn.setAttribute('disabled', '');  
+    }
+
+    const players = 
+    {
         playerName:playerName
     }
     playersArr.push(players);
-    
     display(playersArr);
 }
