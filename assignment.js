338b8d315ed9e90228a41c3fb2e00450e28b5d8b
riazul78 <riazulrakib@gmail.com>
2022-08-23 18:32:41 +0600

    player-cost-calculation

--- assignment.js
+++ assignment.js
@@ -4,7 +4,7 @@ function display(playersHere){
     playersList.innerHTML = "";
     for(let i = 0; i < playersHere.length; i++){
            
-             const allPlayers = playersArr[i].playerName;
+             const allPlayers = playersHere[i].playerName;
 
              const li =document.createElement('li');
              li.innerText = allPlayers;
@@ -14,7 +14,7 @@ function display(playersHere){
 }
 function selectPlayer(player){
     const playerName =player.parentNode.children[0].innerText;
-    
+
     if (playersArr.length >= 5) {
         alert('maximum players added');
         const ol = document.getElementById('ol li');
@@ -33,3 +33,36 @@ function selectPlayer(player){
     playersArr.push(players);
     display(playersArr);
 }
+
+// calculate-part
+document.getElementById('calculate-btn').addEventListener('click', function(){
+         const perPlayer = document.getElementById('per-player');
+         const perCost = perPlayer.value;
+         const totalCost = parseFloat(perCost)
+
+         const perPlayerTotal = playersArr.length * totalCost;
+
+         const playerExpenses = document.getElementById('player-expenses');
+         playerExpenses.innerText = perPlayerTotal;
+         perPlayer.value = '';
+})
+//  total-budget-part
+document.getElementById('calculate-total-btn').addEventListener('click', function(){
+         const manager = document.getElementById('manager');
+         const managerValue =manager.value;
+         const perManagerCost = parseFloat(managerValue);
+
+         const coach = document.getElementById('coach');
+         const coachValue =coach.value;
+         const perCoachCost = parseFloat(coachValue);
+
+         const playerExpenses = document.getElementById('player-expenses');
+         const expensesValue = playerExpenses.innerText;
+         const convertExpensesToNumber = parseFloat(expensesValue);
+
+         const calculateTotal = convertExpensesToNumber + perManagerCost + perCoachCost;
+        //  console.log(calculateTotal);
+
+        const total = document.getElementById('total');
+        total.innerText = calculateTotal;
+})
